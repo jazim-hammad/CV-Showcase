c72f3d83d44d578115caa284faee06f861471a8d
Jazim hammad <jazimhammad03@icloud.com>
2024-05-28 19:11:52 +0400

    updates

--- script.js
+++ script.js
@@ -6,30 +6,31 @@
       activeClass: "timeline-item--active",
       img: ".timeline__img",
     };
-
-    // Initialize the first item as active and set the background image
     selectors.item.eq(0).addClass(selectors.activeClass);
     selectors.id.css(
       "background-image",
       "url(" + selectors.item.first().find(selectors.img).attr("src") + ")"
     );
-
+    var itemLength = selectors.item.length;
     $(window).scroll(function () {
-      var winHeight = $(this).height(); // Get the height of the viewport
-      var winTop = $(this).scrollTop(); // Get the top scroll position
-      var winBottom = winTop + winHeight; // Calculate the bottom scroll position of the viewport
-
-      selectors.item.each(function () {
-        var itemTop = $(this).offset().top; // Top position of the current item
-        var itemBottom = itemTop + $(this).outerHeight(); // Bottom position of the current item
-
-        // Check if the entire item is within the viewport
-        if (itemTop >= winTop && itemBottom <= winBottom) {
-          // Update background image to current item's image
-          var imageUrl = $(this).find(selectors.img).attr("src");
-          selectors.id.css("background-image", "url(" + imageUrl + ")");
-
-          // Update active class
+      var max, min;
+      var pos = $(this).scrollTop();
+      selectors.item.each(function (i) {
+        min = $(this).offset().top;
+        max = $(this).height() + $(this).offset().top;
+        var that = $(this);
+        if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
+          selectors.item.removeClass(selectors.activeClass);
+          selectors.id.css(
+            "background-image",
+            "url(" + selectors.item.last().find(selectors.img).attr("src") + ")"
+          );
+          selectors.item.last().addClass(selectors.activeClass);
+        } else if (pos <= max - 100 && pos >= min) {
+          selectors.id.css(
+            "background-image",
+            "url(" + $(this).find(selectors.img).attr("src") + ")"
+          );
           selectors.item.removeClass(selectors.activeClass);
           $(this).addClass(selectors.activeClass);
         }
